990e4d745e010fac18e3fca70ebfcdfcda815179
Ben Leedom <40598082+Mivao@users.noreply.github.com>
2021-05-06 11:19:28 +1000

    Very Basic Serilog Setup.

---
+++ Serilog/SerilogExample.API/SerilogExample.API/Controllers/ExampleController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerilogExample.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        private readonly ILogger<ExampleController> _logger;
+
+        public ExampleController(ILogger<ExampleController> _logger)
+        {
+            this._logger = _logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> HelloWorld()
+        {
+            _logger.LogInformation("Hello World get request called");
+            return Ok("Hello!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ExampleException()
+        {
+            _logger.LogInformation("Example Exception post request called");
+            try
+            {
+                throw new Exception("Example exception thrown");
+            } 
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception in ExampleException post call");
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
--- Serilog/SerilogExample.API/SerilogExample.API/Program.cs
+++ Serilog/SerilogExample.API/SerilogExample.API/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,24 @@ namespace SerilogExample.API
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            //Statically log the string to information level in log. No d
+            Log.Information("Application Starting Up");
             CreateHostBuilder(args).Build().Run();
+            //Required to send out the logs.
+            Log.CloseAndFlush();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
