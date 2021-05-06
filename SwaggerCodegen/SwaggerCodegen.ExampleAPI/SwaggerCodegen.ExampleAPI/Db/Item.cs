ea5a6b36df8eeecb6358c2453f3e793c2f287e41
Ben Leedom <40598082+Mivao@users.noreply.github.com>
2021-05-06 14:32:33 +1000

    Used swagger Codegen to generate a class library for an API.

---
+++ SwaggerCodegen/SwaggerCodegen.ExampleAPI/SwaggerCodegen.ExampleAPI/Db/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerCodegen.ExampleAPI.Db
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string ItemData { get; set; }
+    }
+}
